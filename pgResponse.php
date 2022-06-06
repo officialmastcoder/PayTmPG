9f09e55c6d29198ff80f3805676439295ca3e306
AHK WEB SOLUTIONS <96109325+ahkwebsolutions@users.noreply.github.com>
2022-06-06 19:24:21 +0530

    Add files via upload

---
+++ pgResponse.php
@@ -0,0 +1,67 @@
+<?php
+header("Pragma: no-cache");
+header("Cache-Control: no-cache");
+header("Expires: 0");
+
+// following files need to be included
+require_once("./lib/config_paytm.php");
+require_once("./lib/encdec_paytm.php");
+
+// Here your Logic  According to your Need
+$conn = mysqli_connect('localhost','root','','paytm');
+$s= "SELECT * FROM users WHERE username='ahkweb'";
+$res = mysqli_query($conn,$s);
+$udata = mysqli_fetch_assoc($res);
+
+$order = $_POST['ORDERID'];
+
+$sl= "SELECT * FROM payment WHERE order_id='$order'";
+$pres = mysqli_query($conn,$sl);
+$pdata = mysqli_fetch_assoc($pres);
+
+$order_id= $pdata['order_id'];
+
+$paytmChecksum = "";
+$paramList = array();
+$isValidChecksum = "FALSE";
+
+$paramList = $_POST;
+
+
+$paytmChecksum = isset($_POST["CHECKSUMHASH"]) ? $_POST["CHECKSUMHASH"] : ""; //Sent by Paytm pg
+
+//Verify all parameters received from Paytm pg to your application. Like MID received from paytm pg is same as your application�s MID, TXN_AMOUNT and ORDER_ID are same as what was sent by you to Paytm PG for initiating transaction etc.
+$isValidChecksum = verifychecksum_e($paramList, PAYTM_MERCHANT_KEY, $paytmChecksum); //will return TRUE or FALSE string.
+
+
+if($isValidChecksum == "TRUE") {
+	echo "<b>Checksum matched and following are the transaction details:</b>" . "<br/>";
+	if ($_POST["STATUS"] == "TXN_SUCCESS") {
+
+		// if Success  Update Database 
+		$sql = "UPDATE `payment` SET `status`='success' WHERE order_id='$order_id'";
+		$q = mysqli_query($conn,$sql);
+		
+		echo "<b>Transaction status is success</b>" . "<br/>";
+		//Process your transaction here as success transaction.
+		//Verify amount & order id received from Payment gateway with your application's order id and amount.
+	}
+	else {
+		echo "<b>Transaction status is failure</b>" . "<br/>";
+	}
+
+	if (isset($_POST) && count($_POST)>0 )
+	{ 
+		foreach($_POST as $paramName => $paramValue) {
+				echo "<br/>" . $paramName . " = " . $paramValue;
+		}
+	}
+	
+
+}
+else {
+	echo "<b>Checksum mismatched.</b>";
+	//Process transaction as suspicious.
+}
+
+?>
